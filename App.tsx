64d9988d5e2bc4f8117bccced63c757f1d2bd277
oDevBR <33878228+oDevBR@users.noreply.github.com>
2023-01-17 10:42:26 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoading] = useFonts({
+    useFonts,
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (fontsLoading) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>TESTING UPDATE!</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
   );
 }
@@ -13,8 +36,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09090A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
   },
 });
